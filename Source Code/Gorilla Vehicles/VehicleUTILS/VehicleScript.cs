62014a4946203febe515173d49cc58c27019b6fb
Blas1ed <117614416+Blas1ed@users.noreply.github.com>
2024-01-07 21:07:44 -0800

    Updated to V1.0.1

--- Source Code/Gorilla Vehicles/VehicleUTILS/VehicleScript.cs
+++ Source Code/Gorilla Vehicles/VehicleUTILS/VehicleScript.cs
@@ -1,9 +1,15 @@
-﻿using System;
+using GorillaNetworking;
+using HarmonyLib;
+using OculusSampleFramework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using Valve.VR;
 
 // Don't roast my code pls its pretty messy
 // Also hi AHauntedArmy lol if you looking at this
@@ -20,11 +26,12 @@ namespace Gorilla_Vehicles.VehicleUTILS
         public float VehicleSpeedMutiplyer = 500f;
         public bool SelectedVehicle = false;
         public GameObject GorillaPlayer = new GameObject();
-
+        public Transform DrivePoint;
         private float motorInput;
         public float breakForce;
         private float steeringInput;
         private float currentbreakforce;
+        bool rightStickClick;
 
         public void Awake()
         {
@@ -42,6 +49,18 @@ namespace Gorilla_Vehicles.VehicleUTILS
                 motorInput = ControllerInputPoller.instance.rightControllerIndexFloat - ControllerInputPoller.instance.leftControllerIndexFloat;
                 steeringInput = ControllerInputPoller.instance.rightControllerPrimary2DAxis.x;
                 currentbreakforce = ControllerInputPoller.instance.rightControllerSecondaryButton ? breakForce : 0f;
+                GorillaPlayer.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                GorillaPlayer.transform.position = DrivePoint.GetWorldPose().position;
+
+                bool IsSteamVR = Traverse.Create(PlayFabAuthenticator.instance).Field("platform").GetValue().ToString().ToLower() == "steam";
+
+                if (IsSteamVR) { rightStickClick = SteamVR_Actions.gorillaTag_RightJoystickClick.GetState(SteamVR_Input_Sources.RightHand); }
+                else { ControllerInputPoller.instance.rightControllerDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out rightStickClick); }
+
+                if (rightStickClick)
+                {
+                    SelectedVehicle = false;
+                }
             }
             else
             {
@@ -51,11 +70,11 @@ namespace Gorilla_Vehicles.VehicleUTILS
         }
 
         public void FixedUpdate()
-        {         
-                ApplyMotorTorque();     
-                ApplySteeringAngle();
-                UpdateWheels();
-                ApplyBreakForce();
+        {
+            ApplyMotorTorque();
+            ApplySteeringAngle();
+            UpdateWheels();
+            ApplyBreakForce();
         }
 
         void ApplyMotorTorque()
@@ -92,5 +111,6 @@ namespace Gorilla_Vehicles.VehicleUTILS
     ; wheelCollider.GetWorldPose(out pos, out rot);
             wheelTransform.rotation = rot;
         }
+
     }
 }
